da05f01828bd6a400a7ca2121649d2f76b4c738a
shruts37 <shruti3219@gmail.com>
2023-05-21 22:11:30 +0530

    commit

---
+++ book-my-show Project/planning.js
@@ -0,0 +1,23 @@
+// Book my show planning
+
+// ->  reusable carousel
+//   -> Hero carousel
+//   -> Common carousel
+//   -> cast and crew small carousel
+
+
+// Pages
+// -> Home Page
+// -> Movie Page
+// -> Categories Page
+// -> Auth Page
+// -> Razorpay payment gateway
+
+// Plan
+// Layouts
+// Routes
+// Pages 
+// Components
+
+// HOC
+// Higher Order Components
